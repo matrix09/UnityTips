690752e103f9c9a92ede6ed9c86d67bb30ceee2f
Erric <mateixzxf@icloud.com>
2017-11-10 11:05:15 +0800

    提交技术小贴士

---
+++ Assets/UGUI-Slider/Slider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour {
+
+    public void SetFloat(float _f)
+    {
+        Debug.Log(_f);
+    }
+
+}
---
+++ Assets/UGUI-Slider/readme.txt
@@ -0,0 +1,3 @@
+Slider -> Event Callback
+
+using dynamic list when you choose your callback function
